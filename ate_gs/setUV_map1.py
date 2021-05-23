7a564c4cc6ea81b6956bc9a7d16bbe98105510c4
alexrcinta <alexrcinta@gmail.com>
2021-05-23 17:08:51 +0200

    Set UVchannel script updated

--- ate_gs/setUV_map1.py
+++ ate_gs/setUV_map1.py
@@ -8,7 +8,7 @@ def setUV_map1 ():
     for item in selection:
         # Variables
         shape = item + 'Shape'
-        nodeTexture = 'TX_' + item
+        nodeTexture = 'TX_' + item[:-5]
 
         mc.uvLink( uvSet = shape + '.uvSet[0].uvSetName', texture = nodeTexture )
 
--- ate_gs/setUV_uvSet1.py
+++ ate_gs/setUV_uvSet1.py
@@ -1,20 +1,73 @@
 import maya.cmds as mc
 
-def setUV_uvSet1 ():
+selection = mc.ls(selection = True)
+channelslist = []
 
-    selection = mc.ls(selection = True)
+for mesh in selection:
+	shape = mesh + 'Shape'
+	
+	for UVchannel in range(0,10):
+		uvset = mc.getAttr(shape + '.uvSet[' + str(UVchannel) + '].uvSetName')
+		
+		channelslist.append(uvset)
+
+for item in selection:
+    shape = item + 'Shape'
+    nodeTexture = 'TX_' + item[:-5]
+    try:
+        uvset1 = channelslist.index('uvSet1')
+        mc.uvLink( uvSet = shape + '.uvSet[' + str(uvset1) + '].uvSetName', texture = nodeTexture)
+        print ('uvSet1 set successfully!'),
+    except ValueError:
+        print ('This mesh has not a second UV channel'),
 
 
-    for item in selection:
-        # Variables
-        shape = item + 'Shape'
-        nodeTexture = 'TX_' + item
 
-        try:
-            switcher = mc.uvLink( uvSet = shape + '.uvSet[1].uvSetName', texture = nodeTexture )
 
-        except TypeError:
-            print('Has selecionado alguna geometria que no tiene un segundo canal de UVs denominado uvSet1. Ver Script Editor para mas info.\n'),
-            print item, 'no tiene un segundo canal de UVs denominado uvSet1.'
 
-setUV_uvSet1()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import maya.cmds as mc
+
+selection = mc.ls(selection = True)
+channelslist = []
+
+for mesh in selection:
+	shape = mesh + 'Shape'
+	
+	for UVchannel in range(0,10):
+		uvset = mc.getAttr(shape + '.uvSet[' + str(UVchannel) + '].uvSetName')
+		
+		channelslist.append(uvset)
+
+for item in selection:
+    shape = item + 'Shape'
+    nodeTexture = 'TX_' + item[:-5]
+    try:
+        uvset1 = channelslist.index('uvSet1')
+        mc.uvLink( uvSet = shape + '.uvSet[' + str(uvset1) + '].uvSetName', texture = nodeTexture)
+        print ('uvSet1 set successfully!'),
+    except ValueError:
+        print ('This mesh has not a second UV channel'),
